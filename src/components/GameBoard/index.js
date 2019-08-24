c5172e7e8e304aeb325c3a1de2ca475482845079
jonhadi <hadijonathan@gmail.com>
2019-08-24 10:05:54 -0700

    images working, front end is left to do

--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -4,7 +4,10 @@ import "./style.css";
 function GameBoard(props) { 
 
     return (
-        <div id={props.id} className="col-3 image-card "><img onClick={() => props.shuffle(props.id)} alt={props.name} src={props.image}/></div>
+        <div id={props.id} className="image-card col-3">
+            <img className="card-img-top" onClick={() => props.shuffle(props.id)} src={props.image} alt={props.name} />
+        </div>
+
     )
 }
 export default GameBoard;
